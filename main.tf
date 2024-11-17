f6c4037038638bebfd90285e617d742f23cbb960
Amol Ghadge <64397760+Amolghadge@users.noreply.github.com>
2024-11-17 11:32:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-resource "google_storage_bucket" "my-bucket" {
-  name                     = "gcp-tf-githubdemo-bucket-01"
-  project                  = "charged-state-441016-j3"
-  location                 = "US"
-  force_destroy            = true
+resource "google_storage_bucket" "my-bucket1" {
+  name          = "gcp-tf-githubdemo-bucket-01"  
+  project       = "charged-state-441016-j3"
+  location      = "US"  
+  force_destroy = true  
   public_access_prevention = "enforced"
+
 }
 
 resource "google_storage_bucket" "my-bucket2" {
@@ -14,4 +15,10 @@ resource "google_storage_bucket" "my-bucket2" {
   public_access_prevention = "enforced"
 }
 
-
+resource "google_storage_bucket" "my-bucket3" {
+  name                     = "gcp-tf-githubdemo-bucket-03"
+  project                  = "charged-state-441016-j3"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
